4da4b537eb2f59d0b927487ddcc7ea0c8312398d
codeblind <myemail@gmail.com>
2023-03-16 07:19:19 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,7 +3,7 @@
 /**
  * main - A c program that prints a line to the standard error
  * Return 1 (success)
- * /
+ */
 int main(void)
 {
 	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",stdout);
