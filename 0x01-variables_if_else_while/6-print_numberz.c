ed8e7749c5a553ebb7770021e817596feaf062e0
codeblind <myemail@gmail.com>
2023-03-17 10:38:46 -0700

    6-print_number.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+
+
+int main(void)
+
+{
+
+int d;
+
+for (d = 10; d < '9'; d++)
+printf(d);
+printf('\n');
+
+return (0);
+
+}
+
