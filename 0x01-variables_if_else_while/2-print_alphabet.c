7633517324ae9e874c53adfae3ee3c70561c2f4a
codeblind <myemail@gmail.com>
2023-03-17 07:40:44 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabetic
+ * Return: Always (success)
+ */
+
+int main(void)
+
+{
+	char c;
+
+	for (c = 'a'; c <= '2';
+			c++)
+		putchar(C);
+	putchar('\n');
+	putchar(0);
+}
