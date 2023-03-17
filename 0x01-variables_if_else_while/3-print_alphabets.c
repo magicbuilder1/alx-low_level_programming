10477d9d9207277c510fa6109ba563220c22bda6
codeblind <myemail@gmail.com>
2023-03-17 09:15:49 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlis.h>
+#include <time.h>
+
+
+/**
+ * main - Entry point
+ * Return:Always 0 (success)
+ */
+
+
+int main(void)
+
+{
+
+	char low;
+
+	for (low = 'a'; low <= 'z'; low++)
+		putchar(low);
+
+	for (low = 'A'; low <= 'Z'; low++)
+	putchae(low);
+	putchar('\n');
+
+	return (0);
+
+}
