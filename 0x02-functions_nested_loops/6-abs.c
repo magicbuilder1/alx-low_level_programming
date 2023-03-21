d066b4ef6d3ba099bd75ce674fc3c0bf4e229e53
codeblind <myemail@gmail.com>
2023-03-21 05:27:39 -0700

    update

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _abs - computer the absolute value of an integer
+ * @c: the number to be computed
+ * Retrun: Absolute value of number of zero
+ */
+
+int _abs(int c)
+
+{
+
+if (c < 0)
+{
+int abs_val;
+
+abs_val = c * -1;
+return (abs_val);
+}
+return (c);
+}
