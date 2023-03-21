f69bebd565c6a4a8f711b3bc9404d0a2501a313c
codeblind <myemail@gmail.com>
2023-03-21 08:54:25 -0700

    all

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,15 +2,15 @@
 
 /**
  * print_last_digit - prints the last digit of a n
- * @c: the number to be treated
+ * @n: the number to be treated
  * Return: value of the last digit of number
  */
 
-int print_last_digit(int c)
+int print_last_digit(int n)
 {
 int last;
 
-last  = c % 10;
+last  = n % 10;
 if (last < 0)
 {
 last = last * -1;
