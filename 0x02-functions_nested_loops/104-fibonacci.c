22f2c35290f126d7e4ad44ed60b02feabd2224c3
codeblind <myemail@gmail.com>
2023-03-22 01:14:31 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -11,7 +11,7 @@ int main(void)
 
 int count;
 unsigned long fib1 = 0, fib2 = 1, sum;
-unsigned long fib1_half1, fib1_half2, fib1_half1, fib2_half2;
+unsigned long fib1_half1, fib1_half2, fib2_half1, fib2_half2;
 unsigned long half1, half2;
 
 for (count = 0; count < 92; count++)
@@ -22,7 +22,7 @@ fib1 = fib2;
 fib2 = sum;
 }
 fib1_half1 = fib1 / 10000000000;
-fib2_half1 = fib2 / 10000000000;
+fib2_half2 = fib2 / 10000000000;
 fib1_half2 = fib1 % 10000000000;
 fib2_half2 = fib2 % 10000000000;
 for (count = 93; count < 90; count++)
