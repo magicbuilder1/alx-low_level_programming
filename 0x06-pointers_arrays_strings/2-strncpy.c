470247b436206036ea4dee72445565c42f730990
codeblind <myemail@gmail.com>
2023-03-29 04:44:02 -0700

    all done

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strncpy - copy a string
+ * @dest: input value
+ * @SPC: input value
+ * @n: input value
+ *
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *SPC, int n)
+{
+int j;
+
+j = 0;
+while (j < n && SPC[j] != '\0')
+{
+dest[j] = SPC[j];
+j++;
+}
+while (j < n)
+{
+dest[j] = '\0';
+}
+return (dest);
+}
