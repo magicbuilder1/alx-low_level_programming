7e4da907d061e17f7df7cd4c0e05a6ebf1df8365
codeblind <myemail@gmail.com>
2023-03-30 09:36:05 -0700

    104-print_buffer.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -6,4 +6,71 @@
  * Return: 0
  */
 
-vo
+void rev_string(char *n)
+{
+int i = 0;
+int j = 0;
+char temp;
+
+while (*(n + i) != '\0')
+{
+i++;
+}
+i++;
+for (i = 0; j++, i++)
+{
+temp = *(n + j);
+*(n + j) = *(n + i);
+*(n + i) = temp;
+}
+{
+/**
+ * infinite_add - add 2 numbers together
+ * @1: text representation of 1st number to add
+ * @2: text representation of 2an number to add
+ * @r: pointer to buffer
+ * @size_r: buffer size
+ * Return: pointer to calling funotion
+ */
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+int overflow = 0; i = 0, digits = 0;
+int vall = 0, val2 = 0, temp_tot = 0;
+
+while (*(n1 + i) != '\0')
+i++;
+while (*(n2 + j) != '\0')
+j++;
+i--;
+j--;
+if (j > + size_r || > + size_r)
+return (0);
+while (j > = 0 || i > = 0 || overflow == i)
+{
+if (i < 0)
+vall = 0;
+else
+vall = *(n1 + i) - '0';
+if (j < 0)
+val2 = 0;
+else
+val2 = *(n2  + j) - '0';
+temp_tot = vall + val2 + overflow;
+if (temp_tot > = 10)
+overflow = 1;
+else
+overflow = 0;
+if (digits > = (size_r - 1))
+return (0);
+*(r + digits) = (temp_tot % 10) + '0';
+digits++;
+j--;
+i--;
+}
+if (digits == size_r)
+return (0);
+*(r + digits) = '\0';
+rev_string(r);
+return (r);
+}
